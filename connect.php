37fbefbb4038b15f88a0017518a5e7e180a3cbcc
Yassin02 <fastani.yassine@gmail.com>
2017-11-27 21:32:01 +0100

    ajout de connect

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+
+	try {
+		$conn = new PDO(
+			"mysql:host=localhost;dbname=db_notation;charset=UTF8",
+			"root",
+			"");
+	}
+	catch(PDOException $e) {
+		error_log($e->getMessage());
+		die("A database error was encountered");
+	}
+
+
+?>
