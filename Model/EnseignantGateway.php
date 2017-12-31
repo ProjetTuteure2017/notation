c1a3086a56d2118b37abdfa698f1a768534c6be4
Yassin02 <fastani.yassine@gmail.com>
2017-12-31 13:32:00 +0100

    ajout authentification
    reste a faire Model et Views

---
+++ Model/EnseignantGateway.php
@@ -0,0 +1,88 @@
+<?php
+
+class EnseignantGateway {
+
+	public function Login($nom, $motDePasse)
+	{
+		include '../notation/connect.php';
+		
+		try {
+			$nom = isset($_POST['nom']) ? $_POST['nom'] : '';
+			$motDePasse = isset($_POST['motDePasse']) ? $_POST['motDePasse'] : '';
+
+			$query = $conn->prepare("SELECT * FROM personne WHERE nom=:NOM");
+
+			$query->execute(array("NOM"=>$nom));
+			$num = $query->rowCount();
+
+			$checkMDP = $nom;
+			if($num > 0)
+			{
+				if($motDePasse==$checkMDP))
+				{
+					$_SESSION['nom'] = $nom;
+					print 'element exist';
+					return 1;
+				} else 
+				{
+					print 'Mot de passe incorrect';
+				}
+			}
+			else
+			{
+				print 'element doesnt exist';
+			}
+		} catch (Exception $e) {
+            print 'caught exception'. $e->getMessage();
+		}
+	}
+
+	public function IsLogged()
+	{
+		include '../notation/connect.php';
+		
+		$nomCheck = isset($_SESSION['nom']) ? $_SESSION['nom'] : '';
+
+		$result = $conn->prepare("SELECT * FROM personne WHERE nom= :NOMCHECK");
+        $result->execute(array("NOMCHECK"=>$nomCheck));
+
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+        $num = $result->rowCount();
+
+        $nomSession =$row['nom'];
+
+        
+        $_SESSION['id'] = $row['id'];
+		
+		if($num > 0)
+        {
+            print 'Session nom' .$nomSession. 'id : '.$_SESSION['id'];
+            return $_SESSION['id'];
+        }
+        else
+        {
+            $conn = null; 
+            print '------not logged in-------';
+            return;   
+        }
+
+	}
+
+	public function IsResponsable()
+	{
+		include '../notation/connect.php';
+
+		$nomCheck = isset($_SESSION['nom']) ? $_SESSION['nom'] : '';
+		
+		$result = $conn->prepare("SELECT * from enseignant e INNER JOIN personne p on e.personneId = p.id WHERE p.nom = :NOMCHECK");
+		$result->execute(array("NOMCHECK"=>$nomCheck));
+		$row = $result->fetch(PDO::FETCH_ASSOC);
+
+        $estResponsable = $row['responsable'];
+
+        return $estResponsable;
+	}
+
+}
+
+?>
---
+++ Model/EnseignantService.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once 'Model/EnseignantGateway.php';
+require_once 'Model/ValidationException.php';
+require_once '../notation/connect.php';
+
+class EnseignantService {
+
+	private $enseignantGateway = NULL;
+
+	function __construct()	{
+		$this->enseignantGateway = new EnseignantGateway();
+	}
+
+	public function EnseignantLogin($nom, $motDePasse) {
+		try {
+			$result = $this->enseignantGateway->Login($nom, $motDePasse);
+			return $result;
+		} catch (Exception $e) {
+			throw $e;
+		}
+	}
+
+	public function EnseignantLoginExist($nom, $motDePasse) {
+
+		try {
+			$this->enseignantGateway->Login($nom, $motDePasse);
+			$result = $this->enseignantGateway->IsLogged();
+			return $result;
+		} catch (Exception $e) {
+			throw $e;
+		}
+	}
+
+	public function ResponsableLogin($nom, $motDePasse) {
+		
+		try {
+			$this->enseignantGateway->Login($nom, $motDePasse);
+			$result = $this->enseignantGateway->IsResponsable();
+			return $result;
+
+		} catch (Exception $e) {
+			throw $e;
+		}
+	}
+
+	public function IsResponsableSession() {
+
+		try {
+			$result = $this->enseignantGateway->IsResponsable();
+			return $result;
+		} catch (Exception $e) {
+			throw $e;
+		}
+	}
+
+}
+
+?>
