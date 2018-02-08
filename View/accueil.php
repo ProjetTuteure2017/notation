5df8c5a2953c1177eb78ab6f9c7fd1a477a96332
bou24 <33953934+bou24@users.noreply.github.com>
2018-02-08 16:43:28 +0100

    paragraphe_acceuil

--- View/accueil.php
+++ View/accueil.php
@@ -8,5 +8,6 @@
 <body>
 
 	<h1>Accueil</h1>
+	<p>Bienvenu dans l'application de notation de projet, cette derniere vous perettera de choisir un groupe  et de le noter mais aussi de creer un projet avec la ou les grilles associees ainsi que les competence a note</p>
 </body>
 </html>
