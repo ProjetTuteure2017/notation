070739b129f7fc27f3e08566e63729e027f611aa
Yassin02 <fastani.yassine@gmail.com>
2018-03-07 22:29:47 +0100

    style button importer

--- View/etudiants.php
+++ View/etudiants.php
@@ -10,13 +10,14 @@
 
 <body>
 	<div class="container">
+    <div class="row">
     <?php 
       sec_session_start();
       if($check == true) {
         $enseignantId = isset($_SESSION['id'])? $_SESSION['id']:NULL;
     ?>
 
-		<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+		<div class="col-12 col-sm-12 col-md-8 col-lg-8">
       <h4>S&eacute;l&eacute;ctionnez un projet :</h4>
       <form method="post" action="">
         <div class="input-group">
@@ -38,12 +39,81 @@
         document.getElementById('selectProjet').value = "<?php echo $_POST['selectProjet'];?>";
       </script>
     </div>
-    
+  </div>
+
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <form method="post" action="index.php?page=etudiant" enctype="multipart/form-data" id="fileLoader" style="margin-top: 10px;">
+                <input type="hidden" name="ProjID" value="<?php print (isset($_POST['selectProjet']) ? $_POST['selectProjet'] : "0");?>"/>
+
+                <label for="file-upload" class="upload">
+                <i class="fas fa-cloud-upload-alt"></i> Importer un fichier
+                </label>
+                <input type="file" id="file-upload" name="import"/>
+                <input type="submit" name="submit" value="Charger" class="btn btn-primary" />
+            </form>
+        </div>
+
+  <?php 
+
+    if(isset($_FILES['import']))
+    {
+      echo '+++++'.$_FILES['import']['name'];
+      
+      $content_dir = 'C:\\wamp64\\www\\tmp\\';
+      $name_file = $_FILES['import']['name'];
+      $tmp_file = $_FILES['import']['tmp_name'];
+      if( !move_uploaded_file($tmp_file, $content_dir . $name_file) )
+      {
+      exit("Impossible de copier le fichier dans $content_dir");
+      }
+      echo "Le fichier a bien été uploadé";
+      $fp = fopen($content_dir.$name_file, "r");
+      echo 'Fichier en lecture<br>';
+      
+      //$i=0;
+    $ligne = fgetcsv($fp);
+    $tabInsertion = array();
+    while (!feof($fp)){
+    $ligne = fgetcsv($fp);
+
+      $ProjID = $_POST['ProjID'];
+      
+      $num = count($ligne);
+      list($nom,$prenom,$idGroupe) = explode(';', $ligne[0]);
+      $etudiant = "{\"nom\" : \"".$nom."\",\"prenom\" : \"".$prenom."\",\"pourcentage\" : \"\",\"note\" : \"\"}";
+      
+      
+      $tempo = array('etudiant' => $etudiant, 'idGroupe' => $idGroupe, 'idProjet'=>$ProjID);
+      $modif = false;
+      $i = 0;
+      foreach($tabInsertion as $tab){
+        if($tab['idGroupe'] == $tempo['idGroupe'])
+        {
+          $tabInsertion[$i]['etudiant'] = $tab['etudiant'] . "," . $tempo['etudiant'];
+          $modif = true;
+        }
+        $i++;
+      }
+      
+      if(!$modif){
+        $tabInsertion = array_merge($tabInsertion, array($tempo));
+      }
+      
+    }
+    echo var_dump($tabInsertion);
+    foreach($tabInsertion as $res){
+      $this->AddGroupe("[".$res['etudiant']."]", "Groupe ".$res["idGroupe"], $res['idProjet']);
+    }
+  }
+  ?>
+    </div>
+    <div class="row">
     <div class="col-lg-12 col-md-12">
       <hr>
       <h4>S&eacute;l&eacute;ctionnez un groupe d'&eacute;tudiant pour la notation :</h4>
       <table class="table table-responsive table-hover">
-        <thead class="indigo">
+        <thead class="color-primary-dark">
           <tr class="text-white">
             <th style="width : 30%">Groupes</th>
             <th style="width: 50%">Noms des &eacute;tudiants</th>
@@ -66,68 +136,11 @@
           </tr>   
         </tbody>
       </table>
+    </div>
   </div>
 
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-  <form method="post" action="index.php?page=etudiant" enctype="multipart/form-data" id="fileLoader">
-	<input type="hidden" name="ProjID" value="<?php print (isset($_POST['selectProjet']) ? $_POST['selectProjet'] : "0");?>"/>
-    <input type="file" name="import"/>
-    <input type="submit" name="submit" value="Charger" />
-  </form>
-  <?php 
 
-    if(isset($_FILES['import']))
-    {
-		  echo '+++++'.$_FILES['import']['name'];
-		  
-		  $content_dir = 'C:\\wamp64\\www\\tmp\\';
-		  $name_file = $_FILES['import']['name'];
-		  $tmp_file = $_FILES['import']['tmp_name'];
-		  if( !move_uploaded_file($tmp_file, $content_dir . $name_file) )
-		  {
-			exit("Impossible de copier le fichier dans $content_dir");
-		  }
-		  echo "Le fichier a bien été uploadé";
-		  $fp = fopen($content_dir.$name_file, "r");
-		  echo 'Fichier en lecture<br>';
-		  
-		  //$i=0;
-		$ligne = fgetcsv($fp);
-		$tabInsertion = array();
-		while (!feof($fp)){
-		$ligne = fgetcsv($fp);
 
-			$ProjID = $_POST['ProjID'];
-			
-			$num = count($ligne);
-			list($nom,$prenom,$idGroupe) = explode(';', $ligne[0]);
-			$etudiant = "{\"nom\" : \"".$nom."\",\"prenom\" : \"".$prenom."\",\"pourcentage\" : \"\",\"note\" : \"\"}";
-			
-			
-			$tempo = array('etudiant' => $etudiant, 'idGroupe' => $idGroupe, 'idProjet'=>$ProjID);
-			$modif = false;
-			$i = 0;
-			foreach($tabInsertion as $tab){
-				if($tab['idGroupe'] == $tempo['idGroupe'])
-				{
-					$tabInsertion[$i]['etudiant'] = $tab['etudiant'] . "," . $tempo['etudiant'];
-					$modif = true;
-				}
-				$i++;
-			}
-			
-			if(!$modif){
-				$tabInsertion = array_merge($tabInsertion, array($tempo));
-			}
-			
-		}
-		echo var_dump($tabInsertion);
-		foreach($tabInsertion as $res){
-			$this->AddGroupe("[".$res['etudiant']."]", "Groupe ".$res["idGroupe"], $res['idProjet']);
-		}
-	}
-  ?>
-</div>
 
 
     <?php
